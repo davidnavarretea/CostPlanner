19947c3b86ff81e69c9aa2635ca442a9954a70d7
David <davidnavarretea1996@gmail.com>
2022-06-14 20:37:26 +0200

    Default value

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,59 +1,79 @@
+import { useState } from 'react'
 import closeModalImg from '../img/cerrar.svg'
 const Modal = ({setModal, animateModal, setAnimateModal}) => {
-const handleModal = () => {
-    setAnimateModal(false)
-    setTimeout(() => {
-      setModal(false)
-    }, 400)
-}
-  return (
-    <div className="modal">
-        <div className="cerrar-modal">
-            <img
-            src={closeModalImg}
-            alt="closeModalImg"
-            onClick={handleModal}
-            />
+    // UseStates
+    const [name,setName] = useState('')
+    const [amount, setAmount] = useState('')
+    const [category, setCategory] = useState('')
+    // Close Modal
+    const handleModal = () => {
+        setAnimateModal(false)
+        setTimeout(() => {
+        setModal(false)
+        }, 400)
+    }
+    // Validation Form
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log('david')
+    }
+    return (
+        <div className="modal">
+            <div className="cerrar-modal">
+                <img
+                src={closeModalImg}
+                alt="closeModalImg"
+                onClick={handleModal}
+                />
+            </div>
+            <form
+                onSubmit={handleSubmit}
+                className={`formulario ${animateModal ? 'animar' : 'cerrar'}`}>
+                <legend>New Expense</legend>
+                <div className='campo'>
+                    <label htmlFor='name'>Name</label>
+                    <input
+                    id='name'
+                    type='text'
+                    placeholder='Rent'
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    />
+                </div>
+                <div className='campo'>
+                    <label htmlFor='amount'>Amount</label>
+                    <input
+                    id='amount'
+                    type='number'
+                    placeholder='500'
+                    value={amount}
+                    onChange={e => setAmount(e.target.value)}
+                    />
+                </div>
+                <div className='campo'>
+                    <label htmlFor='category'>Category</label>
+                    <select
+                    id='category'
+                    value={category}
+                    //defaultvalue
+                    onChange={e => setCategory(e.target.value)}
+                    >
+                        <option value='selected' disabled>Select</option>
+                        <option value='saving'>Saving</option>
+                        <option value='food'>Food</option>
+                        <option value='rent'>Rent</option>
+                        <option value='leisure'>Leisure</option>
+                        <option value='health'>Health</option>
+                        <option value='subscriptions'>Subscriptions</option>
+                        <option value='other'>Other expenses</option>
+                    </select>
+                </div>
+                <input
+                type='submit'
+                value='Add'
+                />
+            </form>
         </div>
-        <form className={`formulario ${animateModal ? 'animar' : 'cerrar'}`}>
-            <legend>New Expense</legend>
-            <div className='campo'>
-                <label htmlFor='name'>Name</label>
-                <input
-                id='name'
-                type='text'
-                placeholder='Rent'
-                />
-            </div>
-            <div className='campo'>
-                <label htmlFor='amount'>Amount</label>
-                <input
-                id='amount'
-                type='number'
-                placeholder='500'
-                />
-            </div>
-            <div className='campo'>
-                <label htmlFor='category'>Category</label>
-                <select
-                id='category'
-                >
-                    <option value='' selected disabled>Select</option>
-                    <option value='saving'>Saving</option>
-                    <option value='food'>Food</option>
-                    <option value='rent'>Rent</option>
-                    <option value='leisure'>Leisure</option>
-                    <option value='health'>Health</option>
-                    <option value='subscriptions'>Subscriptions</option>
-                    <option value='other'>Other expenses</option>
-                </select>
-            </div>
-            <input
-            type='submit'
-            value='Add'
-            />
-        </form>
-    </div>
   )
 }
 
