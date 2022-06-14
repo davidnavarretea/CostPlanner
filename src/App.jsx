5921e91ed34cced35e20b0703495967cdfc1ffa3
David <davidnavarretea1996@gmail.com>
2022-06-14 13:57:47 +0200

    Form validation

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 import Header from './components/Header'
 
 const App = () => {
+  const [budget, setBudget] = useState(0)
   return (
-    <div>
-      <Header/>
-    </div>
+    <>
+      <Header
+      budget = {budget}
+      setBudget = {setBudget}
+      />
+    </>
   )
 }
 
